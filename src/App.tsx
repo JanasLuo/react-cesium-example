71655a5138484bf6cb8908f1a105899e529aad8e
janasluo <janas_luo@126.com>
2021-04-23 15:08:00 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: luolei
+ * @Date: 2021-04-23 11:06:16
+ * @LastEditors: luolei
+ * @LastEditTime: 2021-04-23 14:46:31
+ */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CesiumMap } from './lib/map';
+import Map from './lib/testmap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '100%', width: '100%' }}>
+      {/* <CesiumMap></CesiumMap> */}
+      <Map></Map>
     </div>
   );
 }
